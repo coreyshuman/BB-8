4c44dcf436dfbac4d6f75b925a47d6b8c2e9c848
coreyshuman <ctshumancode@gmail.com>
2015-05-27 01:51:04 -0700

    Added motor controller file, started motor routines devel

---
+++ BB-8.X/motor_controller.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   motor_controller.h
+ * Author: Corey
+ *
+ * Created on May 26, 2015, 8:29 PM
+ */
+
+#ifndef MOTOR_CONTROLLER_H
+#define	MOTOR_CONTROLLER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MOTOR_CONTROLLER_H */
+
