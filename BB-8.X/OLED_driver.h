cfe0c37272c31ba23399678996194f9e724f3953
coreyshuman <ctshumancode@gmail.com>
2015-09-08 22:52:56 -0700

    added oled driver

---
+++ BB-8.X/OLED_driver.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   OLED_driver.h
+ * Author: Corey
+ *
+ * Created on September 4, 2015, 8:39 PM
+ */
+
+#ifndef OLED_DRIVER_H
+#define	OLED_DRIVER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void  OLED_init();
+void OLED_clear();
+void OLED_logo();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* OLED_DRIVER_H */
+
